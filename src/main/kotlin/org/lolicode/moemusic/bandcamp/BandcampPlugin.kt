c662a0a44d5d1bbad4ccecba9a8dcddf4602f215
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2026-08-17 09:17:25 +0000

    build: version bump

--- src/main/kotlin/org/lolicode/moemusic/bandcamp/BandcampPlugin.kt
+++ src/main/kotlin/org/lolicode/moemusic/bandcamp/BandcampPlugin.kt
@@ -14,8 +14,8 @@ object BandcampPlugin : Plugin {
     override val id: String = PLUGIN_ID
     override val configId: String = CONFIG_ID
     override val displayName: LocalizedText = LocalizedText.key("plugin.bandcamp.source")
-    override val version: String = "1.0.0"
-    override val supportedApiVersions: String = ">=2.0.0 <3.0.0"
+    override val version: String = "1.1.0"
+    override val supportedApiVersions: String = ">=2.2.0 <3.0.0"
 
     override val configSpec: PluginConfigSpec<BandcampConfig> =
         pluginConfigSpec(::BandcampConfig) {
